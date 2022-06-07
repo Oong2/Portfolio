c35913bfcb92c1c2d37dcf30bc4eb964d81d4f47
kiho <ehddns0528@gmail.com>
2022-06-07 15:17:57 +0900

    220607

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ console.clear();
 
 new fullpage('#fullpage', {
   // a링크의 이름과 통일시킨다.
-  anchors: ['section1', 'section2', 'section3', 'section4'],
+  anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
 });
 
+$(document).ready(function() {
+   $(".section > .top-right > .more").click(function(){
+     $(".menu-more-wrap").addClass("active");
+
+     
+   if($("menu-more-wrap").hasClass("active")){
+     $(".menu-more-wrap").removeClass("active");
+   }
+
+   });
+
+   $(".menu-more-wrap > .menu-more").click(function(){
+     $(".menu-more-wrap").removeClass("active");
+   })
+
+});
